4024e3fc920b794c2590bf74e4f5454d8e509a8b
Alex Skorulis <skorulis@gmail.com>
2026-05-15 13:54:50 +1000

    Initial Commit

---
+++ CalisTree/CalisTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalisTreeApp.swift
+//  CalisTree
+//
+//  Created by Alex Skorulis on 15/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct CalisTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
